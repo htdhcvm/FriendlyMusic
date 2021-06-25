ab4717d23008d28dc01b66e63dc4c407209cda99
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-06-25 16:44:10 +0300

    main content not auth user vacancy page

---
+++ client/src/components/elements/VacancyContent/VacancyContent.tsx
@@ -0,0 +1,11 @@
+import './VacancyContent.scss';
+
+const VacancyContent = () => {
+    return (
+        <div className="VacancyContent">
+            <h1>VacancyContent</h1>
+        </div>
+    );
+};
+
+export default VacancyContent;
