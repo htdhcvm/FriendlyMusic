5f10ab8375aeb5ba223d682735418b23638e6395
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-06-25 16:28:57 +0300

    add link on image logo

--- client/src/components/elements/Header/Header.tsx
+++ client/src/components/elements/Header/Header.tsx
@@ -6,7 +6,9 @@ import './Header.scss';
 const Header = () => {
     return (
         <div className="header-main">
-            <img className="logo" src="./assets/logo/logoPart.png" />
+            <Link to="/main">
+                <img className="logo" src="/assets/logo/logoPart.png" />
+            </Link>
             <Link to="/signInSignUp">Войти/Регистрация</Link>
         </div>
     );
