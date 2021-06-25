7bda04047dea583a8b7901d555b0238389892468
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-06-25 16:43:16 +0300

    buttons for vacancy send message and respond

---
+++ client/src/components/elements/ButtonsVacancy/ButtonsVacancy.tsx
@@ -0,0 +1,11 @@
+import './ButtonsVacancy.scss';
+
+const ButtonsVacancy = () => {
+    return (
+        <div className="ButtonsVacancy">
+            <button className="respond">Откликнуться</button>
+        </div>
+    );
+};
+
+export default ButtonsVacancy;
