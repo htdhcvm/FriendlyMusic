d8e9327bd8ee0bc3d87341c27feacf3d1e78ee0a
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-06-25 16:30:26 +0300

    delete console.log

--- client/src/features/ui/action.ts
+++ client/src/features/ui/action.ts
@@ -2,11 +2,9 @@ import { Dispatch } from 'redux';
 import { UiAction, UiActionTypes } from './uiDescription';
 
 export const closeMenu = (dispatch: Dispatch<UiAction>) => {
-    console.log('closeMenu');
     dispatch({ type: UiActionTypes.CLOSEMENU, payload: true });
 };
 
 export const openMenu = (dispatch: Dispatch<UiAction>) => {
-    console.log('openMenu');
     dispatch({ type: UiActionTypes.CLOSEMENU, payload: false });
 };
