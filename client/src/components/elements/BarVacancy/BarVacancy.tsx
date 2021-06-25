1a1233323531d2e3e374dbf4b08a0220e9e495c8
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-06-25 16:33:00 +0300

    bar below header on vacancy page

---
+++ client/src/components/elements/BarVacancy/BarVacancy.tsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+
+import './BarVacancy.scss';
+
+interface Props {
+    idGroup: string;
+    groupName: string;
+    socialLinks: Array<SocialLink>;
+}
+
+interface SocialLink {
+    name: string;
+    link: string;
+    image: string;
+}
+
+const BarVacancy = ({ idGroup, groupName, socialLinks }: Props) => {
+    return (
+        <div className="BarVacancy">
+            <div className="wrapper">
+                <div className="links">
+                    <Link to="/main">Главная</Link>
+                    <span>{'>'}</span>
+                    <Link to={`/group/${idGroup}`}>{groupName}</Link>
+                </div>
+                <div className="social">
+                    {socialLinks
+                        ? socialLinks.map((social) => (
+                              <a
+                                  className="itemSocial"
+                                  key={social.name}
+                                  href={social.link}
+                                  target="_blank"
+                              >
+                                  <img
+                                      className="social-image"
+                                      src={social.image}
+                                      alt={`social item ${social.name}`}
+                                  />
+                              </a>
+                          ))
+                        : null}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BarVacancy;
