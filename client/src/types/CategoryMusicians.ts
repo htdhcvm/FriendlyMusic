36fe3c4fb56ec80f5d5fdc5e11e0f2a2ffa93bb9
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-06-28 17:10:29 +0300

    category musicians type

---
+++ client/src/types/CategoryMusicians.ts
@@ -0,0 +1,59 @@
+type Category =
+    | {
+          type: 'conductor';
+          description: 'Дирижер';
+      }
+    | {
+          type: 'contrabass';
+          description: 'Контрабасист';
+      }
+    | {
+          type: 'drummer';
+          description: 'Барабанщин';
+      }
+    | {
+          type: 'electricGuitarBass';
+          description: 'Басист';
+      }
+    | {
+          type: 'electricGuitarRitm';
+          description: 'Ритм-гитарист';
+      }
+    | {
+          type: 'electricGuitarSolo';
+          description: 'Соло-гитарист';
+      }
+    | {
+          type: 'guitarClassic';
+          description: 'Классический-гитарист';
+      }
+    | {
+          type: 'musicTeacher';
+          description: 'Учитель музыки';
+      }
+    | {
+          type: 'piano';
+          description: 'Пианист';
+      }
+    | {
+          type: 'singer';
+          description: 'Певец';
+      }
+    | {
+          type: 'dj';
+          description: 'Диджей';
+      }
+    | {
+          type: 'violin';
+          description: 'Скрипач';
+      }
+    | {
+          type: 'violinist';
+          description: 'Виолончелист';
+      }
+    | {
+          type: '';
+          description: '';
+      };
+
+export default Category;
