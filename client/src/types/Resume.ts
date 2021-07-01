86c8b047c39787c90cd3fb03d685b2754c918c6b
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-07-01 17:08:02 +0300

    type for entity

---
+++ client/src/types/Resume.ts
@@ -0,0 +1,62 @@
+import Qualification from './Qualification';
+import Social from './Social';
+
+type Resume = {
+    fio: string;
+    title: string;
+    dateBirthday?: string;
+    gander?: string;
+    telephone?: string;
+    address?: string;
+    email?: string;
+    site?: string;
+    avatar?: string;
+    profession?: {
+        type: string;
+        description: string;
+    };
+    musicInstrument?: string;
+    salary?: number;
+    experience?: string;
+    aboutYourSelf?: string;
+    kindActivity?: KindActivity;
+    language?: Array<string>;
+    socialList?: Array<Social>;
+    skills?: Array<string>;
+    quality?: Array<string>;
+    prevWorkList?: Array<Work>;
+    institutionList?: Array<Institution>;
+    coursesList?: Array<Course>;
+};
+
+type KindActivity = {
+    type: string;
+    description: string;
+};
+
+export type Course = {
+    name: string;
+    timeEducation: Period;
+    nameSchool: string;
+};
+
+export type Work = {
+    name: string;
+    position: string;
+    link: string;
+    periodWork: Period;
+};
+
+export type Institution = {
+    name: string;
+    timeEducation: Period;
+    nameFaculty: string;
+    qualification: Qualification;
+};
+
+type Period = {
+    start: string;
+    end: string;
+};
+
+export default Resume;
---
+++ client/src/types/ResumeItemList.ts
@@ -0,0 +1,10 @@
+type ResumeItemList = {
+    id: string;
+    title: string;
+    image: string;
+    userName: string;
+    date: string;
+    price: number;
+};
+
+export default ResumeItemList;
