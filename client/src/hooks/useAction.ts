c4409b55639abf84d762520b79d8a79d8a76385d
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-07-01 17:16:59 +0300

    add resume actions

--- client/src/hooks/useAction.ts
+++ client/src/hooks/useAction.ts
@@ -4,8 +4,9 @@ import { bindActionCreators } from 'redux';
 import * as ActionUi from '../features/ui/action';
 import * as ActionUser from '../features/user/action';
 import * as ActionVacancy from '../features/vacancy/action';
+import * as ActionResumes from '../features/resumes/action';
 
-const actions = { ...ActionUser, ...ActionUi, ...ActionVacancy };
+const actions = { ...ActionUser, ...ActionUi, ...ActionVacancy, ...ActionResumes };
 
 export const useAction = () => {
     const dispatch = useDispatch();
