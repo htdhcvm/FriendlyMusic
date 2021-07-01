a25ba05b28cdf6a9461a7acbdf75f2a655871534
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-07-01 17:19:09 +0300

    resume page

--- client/src/components/pages/Resume/Resume.tsx
+++ client/src/components/pages/Resume/Resume.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-
 import './Resume.scss';
 
+import AuthHeader from '../../elements/AuthHeader/AuthHeader';
+import LeftPanel from '../../elements/LeftPanel/LeftPanel';
+import ResumeContent from '../../elements/ResumeContent/ResumeContent';
+
+import withToggleMenu from '../../hoc/withToggleMenu';
+import withResumeData from '../../hoc/withResumeData';
+
 const Resume = () => {
+    const Content = withResumeData({
+        Component: withToggleMenu({ Component: ResumeContent }),
+    });
     return (
-        <div>
-            <h1>Resume</h1>
+        <div className="Resume">
+            <AuthHeader />
+            <LeftPanel />
+            <Content />
         </div>
     );
 };
