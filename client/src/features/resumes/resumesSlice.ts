4943cfca457499ac38c1f6fcc3670b4b213b30d9
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-07-01 17:07:15 +0300

    reducer resume with get all, get current, clear data current

---
+++ client/src/features/resumes/resumesSlice.ts
@@ -0,0 +1,101 @@
+import { Reducer } from 'redux';
+import ResumeItemList from '../../types/ResumeItemList';
+import Resume from '../../types/Resume';
+
+import { ResumesActions, ResumesActionTypes } from './actionDescription';
+
+interface IState {
+    listResumes: Array<ResumeItemList>;
+    currentResume: Resume;
+}
+
+const initialState: IState = {
+    listResumes: [],
+
+    currentResume: {
+        fio: '',
+        title: '',
+        dateBirthday: '',
+        gander: '',
+        telephone: '',
+        address: '',
+        email: '',
+        site: '',
+        profession: {
+            type: '',
+            description: '',
+        },
+        musicInstrument: '',
+        experience: '',
+        aboutYourSelf: '',
+        avatar: '',
+        salary: 0,
+        kindActivity: {
+            type: '',
+            description: '',
+        },
+        language: [],
+        socialList: [],
+        skills: [],
+        quality: [],
+        prevWorkList: [],
+        institutionList: [],
+        coursesList: [],
+    },
+};
+
+const resumesSlice: Reducer<IState, ResumesActions> = (
+    state = initialState,
+    action
+) => {
+    switch (action.type) {
+        case ResumesActionTypes.GetListResumes:
+            return {
+                ...state,
+                listResumes: action.payload,
+            };
+        case ResumesActionTypes.GetResumeOnId:
+            return {
+                ...state,
+                currentResume: action.payload,
+            };
+        case ResumesActionTypes.ClearResumeDate:
+            return {
+                ...state,
+                currentResume: {
+                    fio: '',
+                    title: '',
+                    dateBirthday: '',
+                    gander: '',
+                    telephone: '',
+                    address: '',
+                    email: '',
+                    site: '',
+                    profession: {
+                        type: '',
+                        description: '',
+                    },
+                    musicInstrument: '',
+                    experience: '',
+                    aboutYourSelf: '',
+                    avatar: '',
+                    salary: 0,
+                    kindActivity: {
+                        type: '',
+                        description: '',
+                    },
+                    language: [],
+                    socialList: [],
+                    skills: [],
+                    quality: [],
+                    prevWorkList: [],
+                    institutionList: [],
+                    coursesList: [],
+                },
+            };
+        default:
+            return { ...state };
+    }
+};
+
+export default resumesSlice;
