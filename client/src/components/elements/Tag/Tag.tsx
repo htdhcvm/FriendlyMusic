8b096095b7aae17da04867a1cc733e2162693e84
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-07-18 11:18:19 +0300

    generic tag

--- client/src/components/elements/Tag/Tag.tsx
+++ client/src/components/elements/Tag/Tag.tsx
@@ -1,21 +1,24 @@
 import './Tag.scss';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
-interface Prop {
-    list: Array<string>;
+interface Prop<T, Q> {
+    id: Q;
+    name: T;
+    handleDeleteElement?: (id: Q) => void;
 }
 
-const Tag = ({ list }: Prop) => {
+function Tag<T, Q>({ id, name, handleDeleteElement }: Prop<T, Q>) {
     return (
-        <div className="tags">
-            {list.map((item) => {
-                return (
-                    <div key={item} className="tag">
-                        {item}
-                    </div>
-                );
-            })}
+        <div className='tag'>
+            <span className='text'>{name}</span>{' '}
+            <HighlightOffIcon
+                onClick={() => {
+                    if (handleDeleteElement) handleDeleteElement(id);
+                }}
+                className='icon-delete'
+            />
         </div>
     );
-};
+}
 
 export default Tag;
