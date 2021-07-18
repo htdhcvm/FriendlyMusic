5972adec814f85f8fe83d4aa438e4dfed67d5658
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-07-18 11:49:20 +0300

    left text for inputs

---
+++ client/src/components/elements/LeftTextForField/LeftTextForField.tsx
@@ -0,0 +1,20 @@
+import './LeftTextForField.scss';
+
+import { FieldTextProp } from '../../../types/component/Field';
+
+function LeftTextForField<T>({ title, description }: FieldTextProp<T>) {
+    return (
+        <>
+            {title ? (
+                <div className='LeftTextForField'>
+                    <span className='title'>{title}</span>
+                    <br />
+                    {description ? (
+                        <span className='description'>{description}</span>
+                    ) : null}
+                </div>
+            ) : null}
+        </>
+    );
+}
+export default LeftTextForField;
